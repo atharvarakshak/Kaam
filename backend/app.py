01c1752c23492a171a1a7ef0e9194542034ab93d
atharvarakshak <atharvarakshak23@gmail.com>
2024-12-06 11:34:02 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -26,4 +26,4 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- frontend/src/pages/TestingPage.jsx
+++ frontend/src/pages/TestingPage.jsx
@@ -94,7 +94,7 @@ function PerformanceForm() {
     console.log(payload)
   
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://antidopingtest.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
